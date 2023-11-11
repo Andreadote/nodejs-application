08b7704958557126dfd7e32627738716a83fed54
Andreadote <andre.mj@yahoo.com>
2023-11-11 07:06:47 -0600

    fix: APP-MSS
    Contributing-to: https://kennedymakhanu1.atlassian.net/browse/<JIRA-TICKET# CLD-10>

---
+++ sonar-project.js
@@ -0,0 +1,18 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://3.99.137.1:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
+	    'sonar.login': 'd1c6391fc2984daccc5b8d429025e89598c3d0d5',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
